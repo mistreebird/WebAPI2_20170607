2ce006fa6d181c07f5f6ac7ebc2143b3b0844c44
KaneWei <mistreebird@gmail.com>
2017-06-08 19:50:42 +0800

    11 練習模型驗證(1)

--- WebAPI2_20170607/Models/ProductPatchModel.cs
+++ WebAPI2_20170607/Models/ProductPatchModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,6 +9,7 @@ namespace WebAPI2_20170607.Models
     public class ProductPatchModel
     {
         public int ProductId { get; set; }
+        [StringLength(30,ErrorMessage = "名稱請勿超過30個字")]
         public string ProductName { get; set; }
         public Nullable<decimal> Price { get; set; }
     }
